9f315cb9a9ce12fd8e7008e0c438c98e6cd7ffe0
afvillav <afvillav@espol.edu.ec>
2022-11-11 19:07:41 -0500

    se agrego la funcion para llenar la tabla

--- proyecto02/scripts/data.js
+++ proyecto02/scripts/data.js
@@ -2,6 +2,25 @@ function cargarTabla(id){
     fetch("https://pokeapi.co/api/v2/pokemon/${id}/")
     .then(response => response.json())
     .then(data =>{
-        
+        tipos=data.types
+        nombre=data.name
+        id=data.id
+        tipo_txt=""
+        if(tipos.length>1){
+            tipo1=data.types[0].type.name
+            tipo2=data.types[1].type.name
+            tipo_txt="${tipo1}/${tipo2}"
+        }else{
+            tipos=data.types[0].type.name
+            tipo_txt="${tipo1}"
+        }
+        let cont_tabla= `<tr>
+        <th scope="row">${id}</th>
+        <td>${nombre}</td>
+        <td>${tipo_txt}</td>
+        </tr>`
+
+        document.querySelector(".pokemons").innerHTML+=cont_tabla
     })
+    .catch(console.error);
 }
