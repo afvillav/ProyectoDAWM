1315a848a67c8f30bfa4d1410d77e386e871fab3
afvillav <afvillav@espol.edu.ec>
2022-11-11 21:03:20 -0500

    funcion para llenar tabla
    se creo la funcion para llenar la tabla de pokemones

--- proyecto02/scripts/data.js
+++ proyecto02/scripts/data.js
@@ -1,26 +1,38 @@
 function cargarTabla(id){
-    fetch("https://pokeapi.co/api/v2/pokemon/${id}/")
+    let contenido=""
+    document.querySelector('.pokemons').innerHTML=contenido
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(response => response.json())
     .then(data =>{
-        tipos=data.types
         nombre=data.name
+        tipos=data.types
         id=data.id
         tipo_txt=""
         if(tipos.length>1){
             tipo1=data.types[0].type.name
             tipo2=data.types[1].type.name
-            tipo_txt="${tipo1}/${tipo2}"
+            tipo_txt=`${tipo1}/${tipo2}`
         }else{
             tipos=data.types[0].type.name
-            tipo_txt="${tipo1}"
+            tipo_txt=`${tipos}`
         }
-        let cont_tabla= `<tr>
-        <th scope="row">${id}</th>
-        <td>${nombre}</td>
-        <td>${tipo_txt}</td>
+        contenido = `<tr>
+          <th scope="row">${id}</th>
+          <td>${nombre}</td>
+          <td>${tipo_txt}</td>
         </tr>`
-
-        document.querySelector(".pokemons").innerHTML+=cont_tabla
+        document.querySelector('.pokemons').innerHTML+=contenido
     })
     .catch(console.error);
 }
+
+window.onload=function llenarPokemones(){
+    document.getElementById("cargar").addEventListener("click", () => {
+        for(let i=1; i<=10; i++){
+            cargarTabla(i);     
+        }
+    })
+}
+
+
+
