c7b6c2edf4d69da8790bfb50f84041d0a0755859
afvillav <afvillav@espol.edu.ec>
2022-11-20 22:27:04 -0500

    se agregaron varias funciones
    se agrego la funcion para podere crear una tarjeta del pokemon y tambien una funcion para mostrar los movimientos de los pokemones

--- proyecto02/PokemonDex/js/admin.js
+++ proyecto02/PokemonDex/js/admin.js
@@ -1,41 +1,124 @@
-// Graph
-var ctx = document.getElementById("myChart");
 
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ],
-    datasets: [
-      {
-        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-        lineTension: 0,
-        backgroundColor: "transparent",
-        borderColor: "#007bff",
-        borderWidth: 4,
-        pointBackgroundColor: "#007bff",
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: false,
-          },
-        },
-      ],
-    },
-    legend: {
-      display: false,
-    },
-  },
-});
+
+function mostrar(){
+  var x = document.getElementById("busqueda").value.toLowerCase();
+  crearTarjeta(x);
+  MostraMovs(x);
+}
+
+function crearTarjeta(nombre){
+    let contenido=""
+    document.querySelector('#tarjetaPoke').innerHTML=contenido
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else if(response.status === 404) {
+        contenido = `<div class="card" style="width: 20rem;">
+        <div class="card-header py-3">
+          <h5 class="mb-0 text-center"><strong>No se encontro el pokemon</strong></h5>
+        </div>
+        </div>`
+        document.querySelector('#tarjetaPoke').innerHTML+=contenido
+      }
+    })
+    .then(data =>{
+        nombre=data.name
+        tipos=data.types
+        id=data.id
+        sprite=data.sprites.front_default
+        peso=data.weight
+        altura=data.height
+        tipo_txt=""
+        if(tipos.length>1){
+            tipo1=data.types[0].type.name
+            tipo2=data.types[1].type.name
+            tipo_txt=`${tipo1}/${tipo2}`
+        }else{
+            tipos=data.types[0].type.name
+            tipo_txt=`${tipos}`
+        }
+        contenido = `<div class="card" style="width: 20rem;">
+        <div class="card-header py-3">
+          <h5 class="mb-0 text-center"><strong>Information</strong></h5>
+        </div>
+        <img src="${sprite}" class="card-img-top" alt="pokemon">
+        <div class="card-body">
+          <h5 class="card-title">${nombre}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Id: ${id}</h6>
+          <h6 class="card-subtitle mb-2 text-muted">Type: ${tipo_txt}</h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Weight: ${peso} hg</li>
+            <li class="list-group-item">Height: ${altura} dm</li>
+          </ul>
+        </div>
+      </div>`
+        document.querySelector('#tarjetaPoke').innerHTML+=contenido
+    })
+    .catch(console.error);
+}
+
+function MostraMovs(nombre){
+  let contenido=""
+  document.querySelector('.infoMov').innerHTML=contenido
+  fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
+  .then(response => response.json())
+  .then(data => {
+    mov=data.moves
+    if(mov.length <= 15){
+      for(let i=0; i<mov.length; i++){
+        movi=mov[i].move.name
+        infoMov(movi)
+      }
+    }else{
+      for(let i=0; i<=15; i++){
+        movi=mov[i].move.name
+        infoMov(movi)
+      }
+    }
+  })
+  .catch(console.error);
+}
+
+function infoMov(nombre){
+  fetch(`https://pokeapi.co/api/v2/move/${nombre}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else if(response.status === 404) {
+      contenido = `<tr>
+      <th scope="row">No se encontro el movimiento</th>
+    </tr>`
+      document.querySelector('.infoMov').innerHTML+=contenido
+    }
+  })
+  .then(data => {
+    nom=nombre
+    tipo=data.type.name
+    clase=data.damage_class.name
+    pp=data.pp
+    descripcion=data.effect_entries[0].short_effect
+    poder=data.power
+    if(poder==null){
+      poder=0;
+    }
+    src=""
+    if(clase=="physical"){
+      src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/e/ef/Physical.png?width=325"
+    }else if(clase=="status"){
+      src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/d/d0/Status.png?width=325"
+    }else if(clase=="special"){
+      src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/2/24/Special.png?width=325"
+    }
+    contenido = `<tr>
+      <th scope="row">${nom}</th>
+      <td>${tipo}</td>
+      <td><img src="${src}" alt="tipo">  ${clase}</td>
+      <td>${pp}</td>
+      <td>${poder}</td>
+      <td>${descripcion}</td>
+    </tr>`
+    document.querySelector('.infoMov').innerHTML+=contenido
+  })
+  .catch(console.error);
+}
