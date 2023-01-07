dd5864463e45a9a4a1ff1788618652d4de56abe8
afvillav <afvillav@espol.edu.ec>
2023-01-07 01:27:32 -0500

    creacion de componentes

--- proyecto03/PokemonDex/src/app/app-routing.module.ts
+++ proyecto03/PokemonDex/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HabilidadComponent } from './components/habilidad/habilidad.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { RegionComponent } from './components/region/region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "pokemon", component: PokemonComponent },
+    { path: "habilidad", component: HabilidadComponent },
+    { path: "inicio", component: InicioComponent },
+    { path: "region", component: RegionComponent },
+    { path: "**", redirectTo: "inicio" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyecto03/PokemonDex/src/app/app.module.ts
+++ proyecto03/PokemonDex/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { RegionComponent } from './components/region/region.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HabilidadComponent } from './components/habilidad/habilidad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    RegionComponent,
+    InicioComponent,
+    HabilidadComponent
   ],
   imports: [
     BrowserModule,
