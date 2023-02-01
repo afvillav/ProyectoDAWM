8697d3eb22033517cc4fc8e02c8e11ded43f3543
afvillav <afvillav@espol.edu.ec>
2023-02-01 01:30:01 -0500

    cambios varios

--- proyecto04/admin/routes/index.js
+++ proyecto04/admin/routes/index.js
@@ -20,4 +20,17 @@ router.get('/customers',async function(req, res, next) {
   res.render('customers', { title: 'Customers', customers: response.data });
 })
 
+/*router.get('/sales',async function(req, res, next) {
+  const URL = 'https://sales-data-609d5-default-rtdb.firebaseio.com/data.json'
+  const config = {
+    proxy: {
+      host: 'localhost',
+      port: 4444
+    }
+  }
+  const response = await axios.get(URL, config);
+    
+  res.render('customers', { title: 'Customers', customers: response.data });
+})*/
+
 module.exports = router;
--- proyecto04/api/app.js
+++ proyecto04/api/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var customerRESTRouter = require('./rest/customer');
+var salesRouter = require('./rest/sales');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/rest/customer', customerRESTRouter);
+app.use('/rest/sales', salesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
